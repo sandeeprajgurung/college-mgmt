611f9cd28c7f38155f5c9bf8ee9d802ba17fa0c0
sandeep <smile2srg@gmail.com>
2023-11-28 20:10:07 +0545

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { DatePicker, Button } from "antd";
+import Sidebar from "./ui/modules/components/sidebar";
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
